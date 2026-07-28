254310c86f9b819c685c0bfde58451e93694c2a5
Pammu9148 <pramodrajkumar9966@gmail.com>
2026-07-28 11:09:28 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,16 @@
+/**
+ * Application Entry Point
+ * 
+ * Initializes React application
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles/index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
